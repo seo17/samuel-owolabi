b45eb48bdb66bb1d9cc28015239fb368a3684e1b
seo17 <ebenmosi17@gmail.com>
2024-12-11 10:18:08 -0700

    added submit button and further optimization of code

--- src/components/ui/ContactForm.tsx
+++ src/components/ui/ContactForm.tsx
@@ -3,10 +3,14 @@ import { contactFormInputInfo } from "@/lib";
 import React from "react";
 
 function ContactForm() {
-  const labelClassName = "text-light-gray";
-  const inputClassName =
-    "bg-light-gray/15 text-[#FFF] rounded-md w-full h-10 p-3 focus:ring focus:ring-orange focus:border-none focus:outline-none ";
-  const textBoxClassName = `${inputClassName} h-28`;
+  const textClassName = "text-[14px] md:text-base";
+  const labelClassName = `text-light-gray ${textClassName} `;
+  const inputClassName = `bg-light-gray/15 text-[#FFF] rounded-md w-full h-10 p-3 focus:ring focus:ring-1 focus:ring-orange focus:border-none focus:outline-none ${textClassName}`;
+  const textBoxClassName = `${inputClassName} ${textClassName} h-28`;
+
+  const handleMessageSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+  };
 
   return (
     <div className="w-full">
@@ -14,7 +18,7 @@ function ContactForm() {
         <div className="flex flex-col sm:flex-row flex-wrap gap-y-4 justify-between w-full">
           {contactFormInputInfo.map((item, index) => (
             <div
-              className={`flex flex-col ${
+              className={`flex flex-col gap-1 ${
                 item.type === "textbox" ? "w-full" : "w-full sm:w-[46%]"
               }`}
               key={index}
@@ -29,7 +33,7 @@ function ContactForm() {
                   />
                 </>
               ) : (
-                <div className="flex flex-col w-full">
+                <div className="flex flex-col gap-1 w-full">
                   <label className={labelClassName}>{item.label}</label>
                   <textarea
                     name="message"
@@ -40,6 +44,13 @@ function ContactForm() {
               )}
             </div>
           ))}
+
+          <button
+            onClick={handleMessageSubmit}
+            className={`bg-orange hover:bg-orange/90 h-10 rounded-md w-full font-medium text-[#FFF] ${textClassName}`}
+          >
+            Submit
+          </button>
         </div>
       </form>
     </div>
